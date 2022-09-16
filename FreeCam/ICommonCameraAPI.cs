b2f52cf5b65f1fcff7cbe5c4d912413ef55042c3
Nick <nicholasconnors@hotmail.ca>
2022-09-16 18:27:30 -0400

    Update game libs, switch from InputHandler dep to CCU

---
+++ FreeCam/ICommonCameraAPI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace FreeCam
+{
+	public interface ICommonCameraAPI
+	{
+		void RegisterCustomCamera(OWCamera OWCamera);
+		(OWCamera, Camera) CreateCustomCamera(string name);
+	}
+}
