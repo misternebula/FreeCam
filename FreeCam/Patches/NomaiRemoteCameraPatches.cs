e91e955b59bebefb844ffa1b180f5478ef68181b
Nick <nicholasconnors@hotmail.ca>
2022-10-03 22:07:05 -0400

    Fix bug when entering freecam when in remote camera

---
+++ FreeCam/Patches/NomaiRemoteCameraPatches.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+
+namespace FreeCam.Patches;
+
+[HarmonyPatch(typeof(NomaiRemoteCamera))]
+internal static class NomaiRemoteCameraPatches
+{
+    // Fixes bug where using the FreeCam from a NomaiRemoteCameraPlatform keeps you stuck in the None input mode
+
+    private static InputMode _oldInputMode;
+
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(NomaiRemoteCamera.Activate))]
+    public static void NomaiRemoteCamera_Activate() => _oldInputMode = OWInput.GetInputMode();
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(NomaiRemoteCamera.Deactivate))]
+    public static void NomaiRemoteCamera_Deactivate() => OWInput.ChangeInputMode(_oldInputMode);
+}
